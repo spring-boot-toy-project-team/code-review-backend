b57b51df1abf1ff9301612d82942d42fc1878a37
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-10-27 12:48:59 +0900

    Feat: GlobalAdvice, BusinessLoginException, ErrorResponse Add

--- code-review/src/main/java/com/codereview/exception/ExceptionCode.java
+++ code-review/src/main/java/com/codereview/exception/ExceptionCode.java
@@ -5,8 +5,6 @@ import lombok.Getter;
 public enum ExceptionCode {
   MEMBER_NOT_FOUND(404, "Member not found"),
   MEMBER_ALREADY_EXISTS(409, "Member already exists"),
-  MEMBER_IS_SLEEP(404, "Member is sleep"),
-  MEMBER_IS_QUIT(404, "Member is quit"),
   FIELD_ERROR(400, "Field Error"),
   CONSTRAINT_VIOLATION_ERROR(400, "Constraint Violation Error"),
   NOT_IMPLEMENTATION(501, "Not Implementation"),
