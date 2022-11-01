076ff83b6689f6a8ecbfa4836ba4af1489fd2a00
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-11-01 22:43:16 +0900

    Refactor: SecurityConfig,WebMvcConfig

--- code-review/src/main/java/com/codereview/common/config/WebMvcConfig.java
+++ code-review/src/main/java/com/codereview/common/config/WebMvcConfig.java
@@ -1,7 +1,6 @@
 package com.codereview.common.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
