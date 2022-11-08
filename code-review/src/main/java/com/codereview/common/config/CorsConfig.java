0c2a7de1fc8bc897cd5a5d25c1a9d25f1996b6cd
Park Yun Taek <yountaek11@gmail.com>
2022-11-08 23:14:05 +0900

    Feat: cors 세팅

--- code-review/src/main/java/com/codereview/common/config/CorsConfig.java
+++ code-review/src/main/java/com/codereview/common/config/CorsConfig.java
@@ -13,8 +13,7 @@ public class CorsConfig {
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-//    config.addAllowedOriginPattern("*");
-    config.addAllowedOrigin("*");
+    config.addAllowedOriginPattern("*");
     config.addAllowedHeader("*");
     config.addAllowedMethod("*");
     source.registerCorsConfiguration("/**", config);
