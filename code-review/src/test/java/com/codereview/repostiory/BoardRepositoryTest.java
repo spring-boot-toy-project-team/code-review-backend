2d8393d075ce79dd5cfb00a8477b03949d66a529
Park Yun Taek <yountaek11@gmail.com>
2022-11-10 01:05:32 +0900

    Feat: Board Test Code 작성

--- code-review/src/test/java/com/codereview/repostiory/BoardRepositoryTest.java
+++ code-review/src/test/java/com/codereview/repostiory/BoardRepositoryTest.java
@@ -1,2 +1,28 @@
-package com.codereview.repostiory;public class BoardRepostioryTest {
+package com.codereview.repostiory;
+
+import com.codereview.board.repository.BoardRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class BoardRepositoryTest {
+  @Autowired
+  private BoardRepository boardRepository;
+
+  @Test
+  @DisplayName("게시판 조회 테스트")
+  void findAllTest() throws Exception {
+    // given
+    int page = 1;
+    int size = 10;
+
+    // when
+
+    // then
+  }
 }
--- code-review/src/test/java/com/codereview/stub/BoardStubData.java
+++ code-review/src/test/java/com/codereview/stub/BoardStubData.java
@@ -1,2 +1,26 @@
-package com.codereview.stub;public class BoardStubData {
+package com.codereview.stub;
+
+import com.codereview.board.entity.Board;
+import com.codereview.helper.RestPage;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+public class BoardStubData {
+
+  public static Board board() {
+    return Board.builder()
+      .boardId(1L)
+      .contents("내용")
+      .title("제목")
+      .build();
+  }
+
+  public static RestPage<Board> boardPageRequest(int page, int size) {
+    return new RestPage<>(new PageImpl<>(List.of(
+      board()
+    ), PageRequest.of(page, size, Sort.by("boardId").descending()), 1));
+  }
 }
