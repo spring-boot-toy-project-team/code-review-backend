0423546b6ec98ba89d906d24262e71ce05254509
Park Yun Taek <yountaek11@gmail.com>
2022-11-13 04:46:17 +0900

    Feat: BoardTags QueryDSL 적용

--- code-review/src/main/java/com/codereview/board/repository/boardTags/BoardTagRepository.java
+++ code-review/src/main/java/com/codereview/board/repository/boardTags/BoardTagRepository.java
@@ -1,2 +1,10 @@
-package com.codereview.board.repository.boardTags;public interface BoardTagsRepostiory {
+package com.codereview.board.repository.boardTags;
+
+import com.codereview.board.entity.BoardTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardTagRepository extends JpaRepository<BoardTag, Long>, BoardTagRepositoryCustom {
+
 }
--- code-review/src/main/java/com/codereview/board/repository/boardTags/BoardTagRepositoryCustom.java
+++ code-review/src/main/java/com/codereview/board/repository/boardTags/BoardTagRepositoryCustom.java
@@ -1,2 +1,13 @@
-package com.codereview.board.repository.boardTags;public interface BoardTagRepositoryCustom {
+package com.codereview.board.repository.boardTags;
+
+import com.codereview.board.entity.BoardTag;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardTagRepositoryCustom {
+  boolean existsByBoardIdAndTagId(long boardId, long tagId);
+
+  List<BoardTag> findAllByBoardId(long boardId);
 }
