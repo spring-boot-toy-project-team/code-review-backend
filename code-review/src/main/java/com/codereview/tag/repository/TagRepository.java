ebe87db480b8ed2afdc32e18d9c46b9688a4715b
Park Yun Taek <yountaek11@gmail.com>
2022-11-13 04:48:15 +0900

    Feat: Tag 관련 기능 구현
    - 태그 저장 및 삭제 기능 추가

--- code-review/src/main/java/com/codereview/tag/repository/TagRepository.java
+++ code-review/src/main/java/com/codereview/tag/repository/TagRepository.java
@@ -1,2 +1,12 @@
-package com.codereview.tag.repository;public interface TagRepository {
+package com.codereview.tag.repository;
+
+import com.codereview.tag.entity.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TagRepository extends JpaRepository<Tag, Long> {
+  Optional<Tag> findByName(String name);
 }
--- code-review/src/main/java/com/codereview/tag/service/TagService.java
+++ code-review/src/main/java/com/codereview/tag/service/TagService.java
@@ -1,2 +1,29 @@
-package com.codereview.tag.service;public class TagService {
+package com.codereview.tag.service;
+
+import com.codereview.common.exception.BusinessLogicException;
+import com.codereview.common.exception.ExceptionCode;
+import com.codereview.tag.entity.Tag;
+import com.codereview.tag.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TagService {
+  private final TagRepository tagRepository;
+
+  public Tag findTagOrSave(Tag tag) {
+    Optional<Tag> optionalTag = tagRepository.findByName(tag.getName());
+    return optionalTag.orElseGet(() -> tagRepository.saveAndFlush(tag));
+  }
+
+  @Transactional(readOnly = true)
+  public Tag findTag(String name) {
+    Optional<Tag> optionalTag = tagRepository.findByName(name);
+    return optionalTag.orElseThrow(() -> new BusinessLogicException(ExceptionCode.TAG_NOT_FOUND));
+  }
 }
