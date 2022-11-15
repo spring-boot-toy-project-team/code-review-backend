a4bb64497c92f64f59e4a917a5b340de9a01144f
Park Yun Taek <yountaek11@gmail.com>
2022-11-15 20:09:15 +0900

    Feat: @Setter 추가

--- code-review/src/main/java/com/codereview/tag/entity/Tag.java
+++ code-review/src/main/java/com/codereview/tag/entity/Tag.java
@@ -4,12 +4,14 @@ import com.codereview.board.entity.BoardTag;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Tag {
 
