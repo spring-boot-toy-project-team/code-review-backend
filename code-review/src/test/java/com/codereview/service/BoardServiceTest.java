83ca8d952fd2c67dfea6296e778055d62b9f3a19
Park Yun Taek <yountaek11@gmail.com>
2022-11-16 15:10:43 +0900

    Feat: 임시 커밋

--- code-review/src/test/java/com/codereview/service/BoardServiceTest.java
+++ code-review/src/test/java/com/codereview/service/BoardServiceTest.java
@@ -1,16 +1,48 @@
 package com.codereview.service;
 
+import com.codereview.board.entity.Board;
+import com.codereview.board.entity.BoardTag;
+import com.codereview.board.repository.board.BoardRepository;
+import com.codereview.board.service.BoardService;
+import com.codereview.board.service.BoardTagService;
+import com.codereview.stub.BoardStubData;
+import com.codereview.stub.BoardTagStubData;
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class BoardServiceTest {
+  @Spy
+  @InjectMocks
+  private BoardService boardService;
+  @Mock
+  private BoardRepository boardRepository;
+  @Mock
+  private BoardTagService boardTagService;
 
   @Test
-  @DisplayName("test")
-  public void test() throws Exception {
+  @DisplayName("게시판 저장 테스트")
+  public void getBoardsTest() throws Exception {
+    // given
+    Board board = BoardStubData.board();
+    BoardTag boardTag = BoardTagStubData.boardTag();
+    board.setBoardTags(List.of(boardTag));
 
+    given(boardRepository.save(Mockito.any(Board.class))).willReturn(board);
+
+    // when
+    Board savedBoard = boardService.createBoard(board);
+
+    // then
+    Assertions.assertThat(board).isEqualTo(savedBoard);
   }
 }
--- code-review/src/test/java/com/codereview/service/BoardTagServiceTest.java
+++ code-review/src/test/java/com/codereview/service/BoardTagServiceTest.java
@@ -1,2 +1,138 @@
-package com.codereview.service;public class BoardTagServiceTest {
+package com.codereview.service;
+
+import com.codereview.board.entity.Board;
+import com.codereview.board.entity.BoardTag;
+import com.codereview.board.repository.board.BoardRepository;
+import com.codereview.board.repository.boardTags.BoardTagRepository;
+import com.codereview.board.service.BoardTagService;
+import com.codereview.stub.BoardStubData;
+import com.codereview.stub.BoardTagStubData;
+import com.codereview.tag.repository.TagRepository;
+import com.codereview.tag.service.TagService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class BoardTagServiceTest {
+  @Spy
+  @InjectMocks
+  private BoardTagService boardTagService;
+
+  @Mock
+  private TagService tagService;
+
+  @Mock
+  private TagRepository tagRepository;
+
+  @Mock
+  private BoardRepository boardRepository;
+
+
+//  private final TagService tagService;
+//  private final BoardTagRepository boardTagRepository;
+//
+
+//
+//  private BoardTag updateBoardTag(BoardTag boardTag) {
+//    boardTag.setTag(tagService.findTagOrSave(boardTag.getTag()));
+//    if(existBoardTagByBoardIdAndTagId(boardTag.getBoard().getBoardId(), boardTag.getTag().getTagId()))
+//      return boardTag;
+//    return boardTagRepository.saveAndFlush(boardTag);
+//  }
+//
+//  @Transactional(readOnly = true)
+//  private boolean existBoardTagByBoardIdAndTagId(long boardId, long tagId) {
+//    return boardTagRepository.existsByBoardIdAndTagId(boardId, tagId);
+//  }
+//
+//  @Transactional(readOnly = true)
+//  public List<BoardTag> findBoardTagsByBoardId(long boardId) {
+//    return boardTagRepository.findAllByBoardId(boardId);
+//  }
+//
+//  public void deleteBoardTag(BoardTag boardTag) {
+//    boardTagRepository.delete(boardTag);
+//  }
+//
+//  /**
+//   * 기존의 BoardTag 업데이트
+//   * @param newBoard: 새로운 BoardTag
+//   * @param oldBoard: 기존의 BoardTag
+//   */
+//  public void deleteOldBoardTag(Board newBoard, Board oldBoard) {
+//    List<String> newTag = getNameOfTagsFromBoard(newBoard);
+//    deleteBoardTagIfNotContainsTagName(oldBoard, newTag);
+//    oldBoard.setBoardTags(newBoard.getBoardTags());
+//  }
+//
+//  /**
+//   * 게시글로부터 태그 이름 추출하는 함수
+//   * @param board: 추출할 게시글
+//   * @return
+//   */
+//  private List<String> getNameOfTagsFromBoard(Board board) {
+//    return board.getBoardTags().stream()
+//      .map(boardTag -> boardTag.getTag().getName())
+//      .collect(Collectors.toList());
+//  }
+//
+//  /**
+//   * 게시글로부터 태그 이름 리스트에 포함되지 않으면 삭제하는 함수
+//   * @param board: 비교할 게시글
+//   * @param tagList: 태그 이름 리스트
+//   */
+//  private void deleteBoardTagIfNotContainsTagName(Board board, List<String> tagList) {
+//    board.getBoardTags().stream()
+//      .filter(boardTag -> !tagList.contains(boardTag.getTag().getName()))
+//      .forEach(this::deleteBoardTag);
+//  }
+//
+//  public void updateMultipleBoardTag(Board board) {
+//    board.setBoardTags(board.getBoardTags().stream()
+//      .map(boardTag -> {
+//        boardTag.setBoard(board);
+//        return this.updateBoardTag(boardTag);
+//      })
+//      .collect(Collectors.toList())
+//    );
+//  }
+
+  @Test
+  @DisplayName("")
+  public void createMultipleBoardTagTest() throws Exception {
+    //  public void createMultipleBoardTag(Board board) {
+//    board.setBoardTags(board.getBoardTags().stream()
+//      .peek(boardTag -> {
+//        boardTag.setBoard(board);
+//        boardTag.setTag(tagService.findTagOrSave(boardTag.getTag()));
+//      })
+//      .collect(Collectors.toList()));
+//  }
+    // given
+    Board board = BoardStubData.board();
+    BoardTag boardTag = BoardTagStubData.boardTag();
+    board.setBoardTags(List.of(boardTag));
+
+    doNothing().when(boardTagService).createMultipleBoardTag(Mockito.any(Board.class));
+
+    // when
+    boardTagService.createMultipleBoardTag(board);
+
+    // then
+
+  }
 }
