79110c052659841e731fcb105da7832d732d2ad3
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-12-01 21:39:17 +0900

    Test: SmsController Test

--- code-review/src/test/java/com/codereview/controller/SmsControllerTest.java
+++ code-review/src/test/java/com/codereview/controller/SmsControllerTest.java
@@ -1,13 +1,16 @@
 package com.codereview.controller;
 
+import com.codereview.helper.WithMockCustomUser;
 import com.codereview.member.entity.Member;
-import com.codereview.member.repository.MemberRepository;
 import com.codereview.security.jwt.JwtTokenProvider;
 import com.codereview.sms.controller.SmsController;
 import com.codereview.sms.dto.SmsRequestDto;
 import com.codereview.sms.entity.Sms;
 import com.codereview.sms.mapper.SmsMapper;
 import com.codereview.sms.service.SmsService;
+import com.codereview.stub.MemberStubData;
+import com.codereview.stub.SmsStubData;
+import com.codereview.util.RandomStringUtils;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,11 +31,13 @@ import java.util.List;
 import static com.codereview.util.ApiDocumentUtils.getRequestPreProcessor;
 import static com.codereview.util.ApiDocumentUtils.getResponsePreProcessor;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
 import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -52,57 +57,98 @@ public class SmsControllerTest {
     private SmsService smsService;
 
     @MockBean
-    private MemberRepository memberRepository;
-
-    @MockBean
     private SmsMapper mapper;
 
     @MockBean
     private JwtTokenProvider jwtTokenProvider;
 
-    private Member member;
+    @Test
+    @DisplayName("핸드폰번호 인증 요청 테스트")
+    @WithMockCustomUser
+    void savePhone() throws Exception {
+        //given
+        SmsRequestDto.SavePhoneDto savePhoneDto = SmsStubData.savePhoneDto();
+        Sms sms = SmsStubData.SavePhoneDtoToSmsDto(savePhoneDto);
+        String content = gson.toJson(savePhoneDto);
 
+        given(mapper.savePhoneToSms(Mockito.any(SmsRequestDto.SavePhoneDto.class))).willReturn(sms);
+        given(smsService.saveMemberPhone(Mockito.any(Sms.class))).willReturn(sms);
 
-//    @Test
-//    @DisplayName("핸드폰번호 인증 요청 테스트")
-//    void savePhone() throws Exception {
-//        //given
-//        SmsRequestDto.SavePhoneDto savePhoneDto = SmsStubData.savePhoneDto();
-//        Sms sms = SmsStubData.SavePhoneDtoToSmsDto(savePhoneDto);
-//        String content = gson.toJson(savePhoneDto);
-//
-//        given(mapper.savePhoneToSms(Mockito.any(SmsRequestDto.SavePhoneDto.class))).willReturn(sms);
-//        given(smsService.saveMemberPhone(Mockito.any(Sms.class))).willReturn(sms);
-//
-//        //when
-//        ResultActions actions = mockMvc.perform(
-//          post("/sms/phone")
-//            .accept(MediaType.APPLICATION_JSON)
-//            .contentType(MediaType.APPLICATION_JSON)
-//            .header("Authorization", "Bearer {ACCESS_TOKEN}")
-//            .content(content)
-//        );
-//        //then
-//        actions
-//          .andExpect(status().isCreated())
-//          .andExpect(jsonPath("$.message").value("OK"))
-//            .andDo(
-//              document(
-//                "phone-save",
-//                getRequestPreProcessor(),
-//                getResponsePreProcessor(),
-//                requestHeaders(headerWithName("Authorization").description("Bearer AccessToken")),
-//                requestFields(
-//                    List.of(
-//                        fieldWithPath("phone").type(JsonFieldType.STRING).description("핸드폰 번호")
-//                    )
-//                ),
-//                responseFields(
-//                        List.of(
-//                            fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지")
-//                        )
-//                )
-//              )
-//            );
-//    }
+        //when
+        ResultActions actions = mockMvc.perform(
+          post("/sms/phone")
+            .accept(MediaType.APPLICATION_JSON)
+            .contentType(MediaType.APPLICATION_JSON)
+            .header("Authorization", "Bearer {ACCESS_TOKEN}")
+            .content(content)
+        );
+        //then
+        actions
+          .andExpect(status().isCreated())
+          .andExpect(jsonPath("$.message").value("OK"))
+            .andDo(
+              document(
+                "phone-save",
+                getRequestPreProcessor(),
+                getResponsePreProcessor(),
+                requestHeaders(headerWithName("Authorization").description("Bearer AccessToken")),
+                requestFields(
+                    List.of(
+                        fieldWithPath("phone").type(JsonFieldType.STRING).description("핸드폰 번호"),
+                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").ignored()
+                    )
+                ),
+                responseFields(
+                        List.of(
+                            fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지")
+                        )
+                )
+              )
+            );
+    }
+
+    @Test
+    @DisplayName("핸드폰 인증 성공")
+    @WithMockCustomUser
+    void phoneValidation() throws Exception {
+        //given
+        Member member = MemberStubData.member();
+        SmsRequestDto.validationPhoneDto validationPhoneDto = SmsStubData.validationPhoneDto();
+        String smsCode = RandomStringUtils.generateAuthNo();
+        String content = gson.toJson(validationPhoneDto);
+
+        doNothing().when(smsService).verifiedBySmsCode(smsCode,member.getMemberId());
+        //when
+        ResultActions actions = mockMvc.perform(
+            post("/sms/phone-validation")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .header("Authorization", "Bearer {ACCESS_TOKEN}")
+                .content(content)
+        );
+        //then
+        actions
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.message").value("SUCCESS"))
+            .andDo(
+                document(
+                "validation-phone",
+                getRequestPreProcessor(),
+                getResponsePreProcessor(),
+                requestHeaders(headerWithName("Authorization").description("Bearer AccessToken")),
+                requestFields(
+                    List.of(
+                        fieldWithPath("smsCode").type(JsonFieldType.STRING).description("인증 코드"),
+                        fieldWithPath("memberId").type(JsonFieldType.NUMBER).description("회원 식별자").ignored()
+                    )
+                ),
+                responseFields(
+                    List.of(
+                        fieldWithPath("message").type(JsonFieldType.STRING).description("결과 메시지")
+                    )
+                )
+                )
+            );
+
+    }
 }
