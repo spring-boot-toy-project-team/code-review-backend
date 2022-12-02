fa40cf46141f0b4ad99eaeff858e9c3ff9b03ec2
Park Yun Taek <yountaek11@gmail.com>
2022-12-02 16:12:54 +0900

    Feat: FindByKeword 제거

--- code-review/src/main/java/com/codereview/board/repository/board/BoardSearchRepository.java
+++ code-review/src/main/java/com/codereview/board/repository/board/BoardSearchRepository.java
@@ -2,10 +2,11 @@ package com.codereview.board.repository.board;
 
 import com.codereview.board.document.BoardDocument;
 import com.codereview.helper.RestPage;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoardSearchRepository extends ElasticsearchRepository<BoardDocument, Long> {
-  RestPage<BoardDocument> findByKeyword(String keyword);
+  RestPage<BoardDocument> findByKeyword(String keyword, Pageable pageable);
 }
