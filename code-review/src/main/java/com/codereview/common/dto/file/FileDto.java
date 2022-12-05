2c29c17e0c40488442660e965c8db4f3961ec110
Park Yun Taek <yountaek11@gmail.com>
2022-12-05 20:07:37 +0900

    Feat: FileDto 생성

--- code-review/src/main/java/com/codereview/common/dto/file/FileDto.java
+++ code-review/src/main/java/com/codereview/common/dto/file/FileDto.java
@@ -1,2 +1,15 @@
-package com.codereview.common.dto.file;public class FileDto {
+package com.codereview.common.dto.file;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FileDto {
+  private String uri;
+  private String path;
 }
