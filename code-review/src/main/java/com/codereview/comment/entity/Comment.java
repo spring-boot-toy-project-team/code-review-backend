9ccb40932cb9caf36bb5b9919892045a8c9c22a5
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-12-06 22:46:02 +0900

    Feat: ManyToOne Member 추가

--- code-review/src/main/java/com/codereview/comment/entity/Comment.java
+++ code-review/src/main/java/com/codereview/comment/entity/Comment.java
@@ -2,6 +2,7 @@ package com.codereview.comment.entity;
 
 import com.codereview.board.entity.Board;
 import com.codereview.common.audit.Auditable;
+import com.codereview.member.entity.Member;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
@@ -22,9 +23,14 @@ public class Comment extends Auditable {
   @JoinColumn(name = "BOARD_ID")
   private Board board;
 
+  @ManyToOne
+  @JoinColumn(name = "MEMBER_ID")
+  private Member member;
+
   @Builder
-  public Comment(Long commentId, String contents){
+  public Comment(Long commentId, String contents, Member member){
     this.commentId = commentId;
     this.contents = contents;
+    this.member = member;
   }
 }
