56d692b8afc73497d6d67e4ff1d4ad7787ea6a73
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-12-06 22:48:12 +0900

    Feat: 댓글 리스트 조회 Dto

--- code-review/src/main/java/com/codereview/comment/dto/CommentResponseDto.java
+++ code-review/src/main/java/com/codereview/comment/dto/CommentResponseDto.java
@@ -1,4 +1,23 @@
 package com.codereview.comment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 public class CommentResponseDto {
+
+  @Data
+  @AllArgsConstructor
+  @NoArgsConstructor
+  @Builder
+  public static class CommentDto {
+    private Long commentId;
+    private String contents;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+  }
+
 }
