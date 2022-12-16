99ed25030e40cfb4a2980063248cbcc5d3cdcb69
Park Yun Taek <yountaek11@gmail.com>
2022-12-16 15:12:14 +0900

    Feat: PageDefault 1페이지 부터 시작하도록 Bean 등록

--- code-review/src/main/java/com/codereview/CodeReviewApplication.java
+++ code-review/src/main/java/com/codereview/CodeReviewApplication.java
@@ -3,7 +3,9 @@ package com.codereview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @EnableCaching
 @EnableJpaAuditing
@@ -14,4 +16,10 @@ public class CodeReviewApplication {
     SpringApplication.run(CodeReviewApplication.class, args);
   }
 
+  @Bean
+  public PageableHandlerMethodArgumentResolverCustomizer customize() {
+    return p -> {
+      p.setOneIndexedParameters(true);
+    };
+  }
 }
