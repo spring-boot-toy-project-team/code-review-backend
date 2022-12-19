9c36850cfce7893a95db3ccac4e073b901827116
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-12-19 15:30:42 +0900

    Test: 댓글 저장 테스트

--- code-review/src/test/java/com/codereview/service/CommentServiceTest.java
+++ code-review/src/test/java/com/codereview/service/CommentServiceTest.java
@@ -1,14 +1,21 @@
 package com.codereview.service;
 
+import com.codereview.comment.entity.Comment;
 import com.codereview.comment.repository.CommentRepository;
 import com.codereview.comment.service.CommentService;
+import com.codereview.stub.CommentStubData;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class CommentServiceTest {
@@ -23,7 +30,16 @@ public class CommentServiceTest {
   @DisplayName("댓글 저장 성공 테스트")
   public void saveSuccessForComment(){
     //given
+    Comment comment = CommentStubData.comment();
+
+    given(commentRepository.save(Mockito.any(Comment.class))).willReturn(comment);
+
     //when
+    Comment savedComment = commentService.createComment(comment);
+
     //then
+    assertThat(comment).isEqualTo(savedComment);
   }
+
+
 }
