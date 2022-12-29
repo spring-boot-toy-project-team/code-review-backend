9da09c7674b45f5f70bb74b690ae40adf047f15b
Dong Keun LIM <86710265+Limdongkeun@users.noreply.github.com>
2022-12-29 15:31:15 +0900

    Test: 댓글 저장 실패 케이스

--- code-review/src/test/java/com/codereview/service/CommentServiceTest.java
+++ code-review/src/test/java/com/codereview/service/CommentServiceTest.java
@@ -3,6 +3,8 @@ package com.codereview.service;
 import com.codereview.comment.entity.Comment;
 import com.codereview.comment.repository.CommentRepository;
 import com.codereview.comment.service.CommentService;
+import com.codereview.common.exception.BusinessLogicException;
+import com.codereview.common.exception.ExceptionCode;
 import com.codereview.stub.CommentStubData;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,11 +15,13 @@ import org.mockito.Mockito;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.lang.reflect.Method;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
 public class CommentServiceTest {
@@ -41,6 +45,21 @@ public class CommentServiceTest {
 
     //then
     assertThat(comment).isEqualTo(savedComment);
+  }
+
+  @Test
+  @DisplayName("댓글 저장 실패 테스트")
+  public void failedSaveComment() throws NoSuchMethodException {
+    //given
+    Comment comment = CommentStubData.comment();
+    long commentId = 1L;
+    long memberId = 2L;
+
+    lenient().doReturn(comment).when(commentService).findVerifiedCommentWithMemberId(commentId,memberId);
+    //when
+    //then
+    lenient().doThrow(new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND))
+            .when(commentService).findVerifiedCommentWithMemberId(commentId,memberId);
   }
 
   @Test
